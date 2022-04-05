8a4d211ff17ace9b8a79ad7344529cbd7fa8df79
Rohit Parakh <rohitparakh4@gmail.com>
2022-04-05 23:01:36 +0530

    added icon links

--- src/App.js
+++ src/App.js
@@ -241,9 +241,9 @@ function App() {
       <div className='header'>
         <div className='logo' ><img src="./logo.png"/></div>
         <div className='icons'>
-            <Icons type="twitter" link="https://twitter.com/"/> 
-            <Icons type="discord" link="https://discord.com/"/> 
-            <Icons type="opensea" link="https://opensea.io/"/>
+            <Icons type="twitter" link="https://twitter.com/yuminfts"/> 
+            <Icons type="discord" link="https://discord.gg/STwwF2Ehau"/> 
+            <Icons type="opensea" link="https://opensea.io/collection/yumiofficial"/>
         </div>
     </div>
     <div className="main">
