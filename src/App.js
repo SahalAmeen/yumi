0fd10e8cd831dac4b8331dda338ec2b1c9b75775
Rohit Parakh <rohitparakh4@gmail.com>
2022-04-06 22:43:18 +0530

    gas limit logic

--- src/App.js
+++ src/App.js
@@ -134,7 +134,8 @@ function App() {
     let cost = CONFIG.WEI_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
-    let totalGasLimit = mintAmount>1? String(gasLimit * mintAmount * 0.6) : String(gasLimit) ;
+    let totalGasLimit = String(gasLimit *mintAmount);
+
     console.log("Cost: ", totalCostWei);
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
